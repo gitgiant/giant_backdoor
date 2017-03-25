7cd1b8cce5c6251ad8e830de0b9b9cf0f2b1df5f
gitgiant <prezidentbush@gmail.com>
2017-03-25 03:20:52 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+header = """
+  ,ad8888ba,  88
+ d8"'    `"8b ""                         ,d
+d8'                                      88
+88            88 ,adPPYYba, 8b,dPPYba, MM88MMM
+88      88888 88 ""     `Y8 88P'   `"8a  88
+Y8,        88 88 ,adPPPPP88 88       88  88
+ Y8a.    .a88 88 88,    ,88 88       88  88,
+  `"Y88888P"  88 `"8bbdP"Y8 88       88  "Y888
+______________________________________________
+Version 0.1     http://www.github.com/gitgiant
+Login Backdoor Diagnostic Tool.
+WARNING: USE AT YOUR OWN RISK!"""
